c9d8b7024da6c8426f52e4f8a4a105c66970aa75
zhaoxinkun <zhaoxinkun1124@gmail.com>
2026-06-15 13:22:58 +0800

    chore: 03HDReact TailwindCss and shadcnUI

---
+++ 03HDReact/hd-base/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-100">Hello React</h1>
+      <div className="bg-red-200">111</div>
+      <div className="border-b-2">222</div>
+      <Button variant="default">Click me</Button>
+    </>
+  )
+}
+
+export default App
---
+++ 03HDReact/hd-base/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
+import * as path from 'node:path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+})
