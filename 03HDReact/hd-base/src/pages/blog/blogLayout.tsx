4b2e027b0d00ef96f0140853dde4949baab2b718
zhaoxinkun <zhaoxinkun1124@gmail.com>
2026-06-17 18:53:07 +0800

    refactor: blog

--- 03HDReact/hd-base/src/pages/blog/blogLayout.tsx
+++ 03HDReact/hd-base/src/pages/blog/blogLayout.tsx
@@ -1,13 +1,4 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
 import { Link, Outlet } from '@tanstack/react-router'
-import classNames from 'classnames'
 
 export function BlogLayout() {
   return (
@@ -22,50 +13,7 @@ export function BlogLayout() {
           <Link to="/blog/live">晚八点直播</Link>
         </div>
       </div>
-      <section className="container m-auto">
-        <Card>
-          <CardHeader>
-            <CardTitle>Small Card</CardTitle>
-            <CardDescription>This card uses the small size variant.</CardDescription>
-          </CardHeader>
-          <CardContent>
-            {Array(10)
-              .fill(null)
-              .map((_, i) => {
-                return (
-                  <HoudunrenPage
-                    className="mb-3"
-                    title="后盾人"
-                    content="晚上直播"
-                    view={100}
-                    key={i}
-                  ></HoudunrenPage>
-                )
-              })}
-          </CardContent>
-        </Card>
-      </section>
       <Outlet />
     </>
-  )
-}
-
-interface Props {
-  title: string
-  content: string
-  view: number
-  className?: string
-}
-
-function HoudunrenPage({ className, title, content, view }: Props) {
-  return (
-    // 计算classname
-    <Card className={classNames(className, 'bg-amber-500')}>
-      <CardHeader>
-        <h2> {title}</h2>
-      </CardHeader>
-      <CardContent>{content}</CardContent>
-      <CardFooter>{view}</CardFooter>
-    </Card>
   )
 }
--- 03HDReact/hd-base/src/pages/blog/front.tsx
+++ 03HDReact/hd-base/src/pages/blog/front.tsx
@@ -1,3 +1,59 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import classNames from 'classnames'
+
 export function Front() {
-  return <>this is Front </>
+  return (
+    <>
+      <section className="container m-auto">
+        <Card>
+          <CardHeader>
+            <CardTitle>Small Card</CardTitle>
+            <CardDescription>This card uses the small size variant.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            {Array(10)
+              .fill(null)
+              .map((_, i) => {
+                return (
+                  <HoudunrenPage
+                    className="mb-3"
+                    title="后盾人"
+                    content="晚上直播"
+                    view={100}
+                    key={i}
+                  ></HoudunrenPage>
+                )
+              })}
+          </CardContent>
+        </Card>
+      </section>
+    </>
+  )
+}
+
+interface Props {
+  title: string
+  content: string
+  view: number
+  className?: string
+}
+
+function HoudunrenPage({ className, title, content, view }: Props) {
+  return (
+    // 计算classname
+    <Card className={classNames(className, 'bg-amber-500')}>
+      <CardHeader>
+        <h2> {title}</h2>
+      </CardHeader>
+      <CardContent>{content}</CardContent>
+      <CardFooter>{view}</CardFooter>
+    </Card>
+  )
 }
